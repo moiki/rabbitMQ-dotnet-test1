7ea2f2691e0b70031f75843222fb4a3fcfe87403
moiki <moiki.reyes@gmail.com>
2022-11-07 00:31:51 +0000

    First communication testing with rabbitMQ

---
+++ Reciever/Reciever.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+void main()
+{
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using var rabbitConn = factory.CreateConnection();
+    using (var channel = rabbitConn.CreateModel())
+    {
+        channel.QueueDeclare(queue:"hello", exclusive: false, durable:false, autoDelete: false, arguments: null);
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received: {message}");
+        };
+        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+        Console.WriteLine("Press a key to finish process...");
+        Console.ReadLine();
+    }
+
+    
+}
+main();
---
+++ Send/Send.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text;
+using RabbitMQ.Client;
+
+void main()
+{
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using var rabbitConn = factory.CreateConnection();
+    using (var channel = rabbitConn.CreateModel())
+    {
+        // Creating a Queue
+        channel.QueueDeclare(queue: "hello", exclusive: false, arguments: null, autoDelete: false);
+        const string message = "Send says: Hello world";
+        var body = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+        Console.WriteLine("[x] Message sent");
+    }
+    Console.WriteLine("Press any  key to finish process...");
+    Console.ReadLine();
+}
+main();
